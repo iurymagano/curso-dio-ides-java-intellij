43a071d02d56b7bc810f5ce9abf9bbf76c2992f1
Iury <iurymagano17@live.com>
2022-05-25 14:44:03 -0300

    first commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,10 +8,10 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
-       /* int a = 5;
+        int a = 5;
         int b = 3;
 
-        System.out.println("Hello World! " + (a+b));*/
+        System.out.println("Hello World! " + (a+b));
 
     }
 }
